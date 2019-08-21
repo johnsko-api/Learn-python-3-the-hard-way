c8fa7263d84e7ead38e795fa0fd5ede5985304fc
John Ko <johnsko.api@gmail.com>
2019-08-21 03:23:28 -0400

    2nd day continuing exercises of book

---
+++ ex13.py
@@ -0,0 +1,8 @@
+from sys import argv
+
+script, first, second, third = argv
+
+print("The script is called:", script)
+print("You first variable is:", first)
+print("Second variable is:", second)
+print("Third is:", third)
